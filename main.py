32e678558cf64f3b07c2a39ac1135797fad92104
ecommerceclaroanalitic <ecommerceclaroanalitic@gmail.com>
2025-11-21 11:07:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def generar_speech_producto(nombre, descripcion=None, beneficios=None, user_name
     Crea un mensaje para popup: breve, natural, convincente, máximo 15 palabras.
     Debe mencionar que esta entre los productos más vendidos y populares.
     No incluyas valores numéricos.
-    No incluir palabras de sobrepromesas
+    No incluir palabras de sobrepromesas y que todo mensaje este relacionado con que es una caracteristica de tienda claro.
     No incluyas explicaciones ni comentarios.
     Se debe incluir que estamos en las fiestas de fin de año y navidad y que genere algun sentimeinto de buena vibra
     Texto final listo para pegar.
